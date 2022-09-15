563149081b7e3f4c38d2b0c2bb7578da949ea7c0
nishtha-dev <nishthasaraswat15@gmail.com>
2022-09-15 09:22:27 +0530

    refactored code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:explore_provider/provider/objectProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'dart:async';
-import 'package:uuid/uuid.dart';
+
+import 'modals/baseClass.dart';
 
 void main() {
   runApp(const MyApp());
@@ -67,70 +68,6 @@ class HomePage extends StatelessWidget {
   }
 }
 
-@immutable
-class BaseClass {
-  final String id;
-  final String lastUpdated;
-
-  BaseClass()
-      : lastUpdated = DateTime.now().toIso8601String(),
-        id = Uuid().v4();
-
-  @override
-  bool operator ==(covariant BaseClass other) => id == other.id;
-
-  @override
-  int get hashCode => id.hashCode;
-}
-
-@immutable
-class CheapObject extends BaseClass {}
-
-@immutable
-class ExpensiveObject extends BaseClass {}
-
-class ObjectProvider extends ChangeNotifier {
-  late String id;
-  late CheapObject _cheapObject;
-  late StreamSubscription _cheapObjectStreamSubs;
-  late StreamSubscription _expensiveObjectStreamSubs;
-  late ExpensiveObject _expensiveObject;
-
-  CheapObject get cheapObject => _cheapObject;
-
-  ExpensiveObject get expensiveObject => _expensiveObject;
-
-  ObjectProvider()
-      : id = const Uuid().v4(),
-        _cheapObject = CheapObject(),
-        _expensiveObject = ExpensiveObject();
-
-  @override
-  void notifyListeners() {
-    id = const Uuid().v4();
-    super.notifyListeners();
-  }
-
-  void start() {
-    _cheapObjectStreamSubs =
-        Stream.periodic(Duration(seconds: 1)).listen((event) {
-      _cheapObject = CheapObject();
-      notifyListeners();
-    });
-
-    _expensiveObjectStreamSubs =
-        Stream.periodic(Duration(seconds: 10)).listen((event) {
-      _expensiveObject = ExpensiveObject();
-      notifyListeners();
-    });
-  }
-
-  void reset() {
-    _cheapObjectStreamSubs.cancel();
-    _expensiveObjectStreamSubs.cancel();
-  }
-}
-
 class ExpensiveWidget extends StatelessWidget {
   const ExpensiveWidget({Key? key}) : super(key: key);
 
@@ -144,7 +81,7 @@ class ExpensiveWidget extends StatelessWidget {
       child: Column(
         children: [
           const Text('Expensive Object'),
-          const Text('Last updtaed'),
+          const Text('Last updated'),
           Text(expensiveObject.lastUpdated)
         ],
       ),
